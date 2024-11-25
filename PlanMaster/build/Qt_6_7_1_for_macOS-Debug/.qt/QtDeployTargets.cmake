f886da720eb389abb0dbd259b6b864b52aab6c81
nkarolczyk <148887265+nkarolczyk@users.noreply.github.com>
2024-11-25 14:11:22 +0100

    cleanup

--- PlanMaster/build/Qt_6_7_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_PlanMaster_FILE /Users/nataliakarolczyk/Desktop/studia III semestr/PlanMaster/PlanMaster/build/Qt_6_7_1_for_macOS-Debug/PlanMaster.app/Contents/MacOS/PlanMaster)
